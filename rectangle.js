6144f51e52e0d1ba32b57897de59b69500574a06
shubhamk840 <shubhamk840@gmail.com>
2020-07-05 17:40:47 +0530

    excersing the callback functions

---
+++ rectangle.js
@@ -0,0 +1,17 @@
+module.exports = (x,y,callback)=>
+{
+    if(x<=0 || y<=0)
+    {
+        setTimeout(()=>callback(new Error("Incorrect dimensions"),null),2000);
+    }
+    else
+    {
+        setTimeout(()=>callback(null,{
+            perimeter: () => 2*(x+y),
+            area: () => x*y
+        }),2000);
+    }
+}
+
+perimeter = (x,y) => 2*(x+y);
+area = (x,y) => x*y;
