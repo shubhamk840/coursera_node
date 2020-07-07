35f8a0fbb274499848f69a601829a618d9c7cd0b
shubhamk840 <shubhamk840@gmail.com>
2020-07-07 09:51:35 +0530

    fewer modification using node js , html and css

--- node_express/index.js
+++ node_express/index.js
@@ -3,7 +3,7 @@ const  http = require('http');
 const morgan = require('morgan');
 const app =  express();
 
-app.use(morgan('dev'));
+app.use(morgan('dev')); // these are the middle wares that are used to simplify the task.
 app.use(express.static(__dirname + '/public'));
 
 app.use((req,res,next)=>
